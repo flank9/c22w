e5f886ce4412a9af10b8ae7b13489cae3818a385
flank9 <lmfao@genocide.fun>
2025-03-15 18:55:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,26 +13,3 @@ document.getElementById('enter-overlay').addEventListener('click', function() {
         this.remove();
     }, 1000);
 });
-
-function createFallingFlakes() {
-  const flakesContainer = document.createElement('div');
-  flakesContainer.classList.add('falling-flakes');
-  document.body.appendChild(flakesContainer);
-
-  setInterval(() => {
-    const flake = document.createElement('div');
-    flake.classList.add('flake');
-
-
-    flake.style.left = `${Math.random() * window.innerWidth}px`;
-    flake.style.animationDuration = `${Math.random() * 3 + 2}s`; 
-
-    flakesContainer.appendChild(flake);
-
-   
-    setTimeout(() => {
-      flake.remove();
-    }, 5000); 
-  }, 200); 
-}
-createFallingFlakes();
