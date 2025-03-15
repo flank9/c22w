476751e9d58842122bbd171b6def5b577f91a950
flank9 <lmfao@genocide.fun>
2025-03-15 17:13:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,21 +13,3 @@ document.getElementById('enter-overlay').addEventListener('click', function() {
         this.remove();
     }, 1000);
 });
-<script>
-
-  document.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
-  });
-
-
-  document.addEventListener("selectstart", function (e) {
-    e.preventDefault();
-  });
-
-
-  document.onkeydown = function (e) {
-    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
-      e.preventDefault();
-    }
-  };
-</script>
