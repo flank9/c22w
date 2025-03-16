e0c7511071eb2e2d03f39a4f73517769afed20c7
flank9 <lmfao@genocide.fun>
2025-03-16 20:43:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Autoplay and set random song on page load
     var songs = ["audio.mp3", "audio2.mp3", "audio3.mp3", "audio4.mp3"];
     var randomSong = songs[Math.floor(Math.random() * songs.length)];
-
+ 
     var audio = document.getElementById('background-music');
     audio.src = randomSong;
     audio.autoplay = true;
